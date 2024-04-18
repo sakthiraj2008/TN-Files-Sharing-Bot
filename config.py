e6f67704d12fab2bec34ade76db33a6da7bedafe
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-04-18 09:10:36 -0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Aloneboy:Aloneboytg@clust
 DB_NAME = os.environ.get("DATABASE_NAME", "Dpbotz")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001980120463"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002008853384"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
