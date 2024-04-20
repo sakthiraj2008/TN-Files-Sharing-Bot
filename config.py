28a354ffbb00f60f5e38c6367f658223771b391d
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-04-20 12:58:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Aloneboy:Aloneboytg@cluster0.nmeelsr.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "Dpbotz")
+DB_NAME = os.environ.get("DATABASE_NAME", "Dp_botz")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002008853384"))
