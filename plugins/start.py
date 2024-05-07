22b6f41c458e8910b5b088987f31c00beec0bf9a
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-05-07 06:56:00 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,8 +20,8 @@ from database.database import add_user, del_user, full_userbase, present_user
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
-    id = message.from_user.id
     await message.react(emoji=random.choice(REACTIONS))
+    id = message.from_user.id
     if not await present_user(id):
         try:
             await add_user(id)
