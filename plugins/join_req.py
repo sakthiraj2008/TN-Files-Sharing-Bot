7bc2555095b16644a0c118d28a8b743f996ce26f
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-05-25 11:35:41 +0530

    Create join_req.py

---
+++ plugins/join_req.py
@@ -0,0 +1,25 @@
+from logging import getLogger
+from pyrogram import Client, filters, enums
+from pyrogram.types import ChatJoinRequest
+from database.join_reqs import JoinReqs
+from config import ADMINS, REQ_CHANNEL
+
+
+db = JoinReqs
+logger = getLogger(__name__)
+
+@Client.on_chat_join_request(filters.chat(REQ_CHANNEL if REQ_CHANNEL else "self"))
+async def join_reqs(client, join_req: ChatJoinRequest):
+
+    if db().isActive():
+        user_id = join_req.from_user.id
+        first_name = join_req.from_user.first_name
+        username = join_req.from_user.username
+        date = join_req.date
+
+        await db().add_user(
+            user_id=user_id,
+            first_name=first_name,
+            username=username,
+            date=date
+        )
