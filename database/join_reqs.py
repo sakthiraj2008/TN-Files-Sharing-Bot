b2bcf3d165234305f165a137e0dfca72403a94cd
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-05-25 11:32:22 +0530

    Update join_reqs.py

--- database/join_reqs.py
+++ database/join_reqs.py
@@ -4,7 +4,7 @@ from config import REQ_CHANNEL
 class JoinReqs:
 
     def __init__(self):
-        from info import JOIN_REQS_DB
+        from config import JOIN_REQS_DB
         if JOIN_REQS_DB:
             self.client = motor.motor_asyncio.AsyncIOMotorClient(JOIN_REQS_DB)
             self.db = self.client["JoinReqs"]
@@ -20,23 +20,3 @@ class JoinReqs:
         else:
             return False
 
-    async def add_user(self, user_id, first_name, username, date):
-        try:
-            await self.col.insert_one({"_id": (user_id),"user_id": (user_id), "first_name": first_name, "username": username, "date": date})
-        except:
-            pass
-
-    async def get_user(self, user_id):
-        return await self.col.find_one({"user_id": (user_id)})
-
-    async def get_all_users(self):
-        return await self.col.find().to_list(None)
-
-    async def delete_user(self, user_id):
-        await self.col.delete_one({"user_id": (user_id)})
-
-    async def delete_all_users(self):
-        await self.col.delete_many({})
-
-    async def get_all_users_count(self):
-        return await self.col.count_documents({})
