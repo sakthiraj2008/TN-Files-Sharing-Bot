818538c0e3059010bf3afe2e6ca85b41df7851b5
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-05-25 00:20:49 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,6 +43,19 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
                 self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/All_Tamil_movies_request for support")
                 sys.exit()
+        if REQ_CHANNEL:
+            try:
+                link = (await self.get_chat(REQ_CHANNEL)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(REQ_CHANNEL)
+                    link = (await self.get_chat(REQ_CHANNEL)).invite_link
+                self.invitelink2 = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/All_Tamil_movies_request for support")
+                sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
