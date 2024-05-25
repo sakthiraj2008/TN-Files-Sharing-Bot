4fd2a20bc1a773e87b15f31cd819cd5feab0d237
Dp_Botz <149217129+Dhineshpavi@users.noreply.github.com>
2024-05-25 01:29:25 -0700

    Update config.py

--- config.py
+++ config.py
@@ -33,8 +33,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "mongodb")
 
 #force sub channel id, if you want enable force sub
 FORCESUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002008853384"))
-FORCESUB_CHANNEL2 = int(os.environ.get("FORCESUB_CHANNEL2", "-1002050387186"))
-FORCESUB_CHANNEL3 = int(os.environ.get("FORCESUB_CHANNEL3", "0"))
+
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
