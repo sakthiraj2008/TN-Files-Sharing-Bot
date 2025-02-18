e1a7c54d842771cdaa2f2195554252d98ffba739
TN Botz <163137175+sakthiraj2008@users.noreply.github.com>
2025-02-17 23:56:21 -0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>■ 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 : <a href='tg://user?id={OWNER_ID}'>𝗜𝘁'𝘀 𝗠𝗲</a>\n■ 𝗟𝗮𝗻𝗴𝘂𝗮𝗴𝗲 : <code>𝗣𝘆𝘁𝗵𝗼𝗻𝟯</code>\n■ 𝗦𝗼𝘂𝗿𝗰𝗲 𝗖𝗼𝗱𝗲 : <a href='https://t.me/DP_BOTZ'>𝗖𝗹𝗶𝗰𝗸</a>\n■ 𝗠𝗼𝘃𝗶𝗲𝘀 : <a href='https://t.me/movies_days'>𝗧𝗮𝗺𝗶𝗹 𝗠𝗼𝘃𝗶𝗲𝘀</a>\n■ 𝗔𝗻𝗶𝗺𝗲𝘀  : <a href='https://t.me/AnimesWorldTamil'>𝗧𝗮𝗺𝗶𝗹 𝗔𝗻𝗶𝗺𝗲𝘀</a>\n■ 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 : <a href='https://t.me/AnimesWorldTamil'>𝐃𝐩_𝐁𝐨𝐭𝐳</a></b>",
+            text = f"<b>■ 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿 : <a href='https://t.me/coding_kakashi_bot'>𝗜𝘁'𝘀 𝗠𝗲</a>\n■ 𝗟𝗮𝗻𝗴𝘂𝗮𝗴𝗲 : <code>𝗣𝘆𝘁𝗵𝗼𝗻𝟯</code>\n■ 𝗦𝗼𝘂𝗿𝗰𝗲 𝗖𝗼𝗱𝗲 : <a href='https://t.me/tn_BOTZ'>𝗖𝗹𝗶𝗰𝗸</a>\n■ Main : <a href='https://t.me/tamil_hq_linkzz'>𝗧𝗮𝗺𝗶𝗹 Animes</a>\n■ 𝗔𝗻𝗶𝗺𝗲𝘀  : <a href='https://t.me/+3b-u5nyTTDAzZjJ'>𝗧𝗮𝗺𝗶𝗹 𝗔𝗻𝗶𝗺𝗲𝘀</a>\n■ 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 : <a href='https://t.me/+3b-u5nyTTDAzZjJ'>TN_Botz</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
